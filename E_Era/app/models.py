78e7b43fe4479940f7fbe9017e8704e4524bc937
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-12-10 18:13:40 +0530

    updated

--- E_Era/app/models.py
+++ E_Era/app/models.py
@@ -4,4 +4,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Product(models.Model):
+    pid=models.TextField()
+    name=models.TextField()
+    specifications=models.CharField(max_length=250)
+    price=models.IntegerField()
+    offer_price=models.IntegerField()
+    brand=models.TextField()
+    color=models.TextField()
+    highlights=models.CharField(max_length=35)
+    warranty=models.TextField()
+    services=models.TextField(max_length=30)
+    stock=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    img=models.FileField()
+    
+class Cart(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    qty=models.IntegerField()
 
+class Buy(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    qty=models.IntegerField()
+    price=models.IntegerField()
+    date=models.DateField(auto_now_add=True)
