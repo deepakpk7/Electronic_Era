1bd8ec4841032dda9aaf512fb446107cd494fa80
deepakpk7 <deepakpk7034@gmail.com>
2024-12-25 17:51:24 +0530

    add

--- E_Era/static/js/main.js
+++ E_Era/static/js/main.js
@@ -22,4 +22,33 @@ document.getElementById("logout-btn").addEventListener("click", function () {
       }
     });
   });
+
+
+  var swiper = new Swiper('.product-swiper', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
+
   
